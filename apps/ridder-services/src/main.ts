bea9d3ffb13a2ae393e8e343e9738431e680e1d7
Youssef B <bachir.benmalek@gmail.com>
2025-08-19 01:42:25 +0200

    dockerfile

--- apps/ridder-services/src/main.ts
+++ apps/ridder-services/src/main.ts
@@ -3,6 +3,6 @@ import { RidderServicesModule } from './ridder-services.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(RidderServicesModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
